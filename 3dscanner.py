8121d096d54cb2bc759174010029f5423b3de456
Matthew Diamond <medman826@gmail.com>
2014-11-14 12:28:18 -0600

    Added a GUI for tuning paramters to StereoSGBM

--- 3dscanner.py
+++ 3dscanner.py
@@ -2,67 +2,26 @@ import cv2
 import numpy as np
 
 
-# FLANN matching variables
-FLANN_INDEX_KDTREE = 0
-TREES = 5
-CHECKS = 100
-KNN_ITERS = 2
-LOWE_RATIO = 0.8
+# StereoSGBM values
+minDisparity = 0
+numDisparities = 64
+SADWindowSize = 5
+P1 = 8 * 3 * SADWindowSize ** 2
+P2 = 32 * 3 * SADWindowSize ** 2
+disp12MaxDiff = -1
+# Block matcher
+stereo = cv2.StereoSGBM(minDisparity, numDisparities, SADWindowSize,
+                        P1, P2, disp12MaxDiff)
 
 
-def _rectify_pair(sift, image_left, image_right):
-    """
-    Rectifies a single set of stereo images
-    """
-    # Get key points for both images, find matches
-    kp_left, desc_left = sift.detectAndCompute(image_left, None)
-    kp_right, desc_right = cv2.SIFT().detectAndCompute(image_right, None)
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=TREES)
-    search_params = dict(checks=CHECKS)
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(desc_left, desc_right, k=KNN_ITERS)
+def displayDepth(name, mat):
+    s = v = (np.ones(mat.shape) * 255).astype(np.uint8)
+    h = ((mat -np.nanmin(mat))/ (np.nanmax(mat)- np.nanmin(mat)) * 255).astype(np.uint8)
+    cv2.imshow(name, cv2.cvtColor(cv2.merge([h,s,v]), cv2.cv.CV_HSV2BGR ))
 
-    # Store all the good matches as per Lowe's ratio test
-    good = []
-    for m, n in matches:
-        if m.distance < LOWE_RATIO * n.distance:
-            good.append(m)
 
-    # Pick out the left and right points from the good matches
-    pts_left = np.float32(
-        [kp_left[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-    pts_right = np.float32(
-        [kp_right[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-    # Compute the fundamental matrix
-    F, mask = cv2.findFundamentalMat(pts_left, pts_right, cv2.FM_RANSAC)
-    pts_left = pts_left[mask.ravel() == 1]
-    pts_right = pts_right[mask.ravel() == 1]
-
-    # Rectify the images
-    width, height, _ = image_left.shape
-    _, h1, h2 = cv2.stereoRectifyUncalibrated(
-        pts_left, pts_right, F, (width, height))
-
-    tx1 = h1[0][2]
-    ty1 = h1[1][2]
-    tx2 = h2[0][2]
-    ty2 = h2[1][2]
-
-    t1 = np.array([[1, 0, -tx1], [0, 1, -ty1], [0, 0, 1]])
-    t2 = np.array([[1, 0, -tx2], [0, 1, -ty2], [0, 0, 1]])
-
-    ht1 = t1.dot(h1)
-    ht2 = t2.dot(h2)
-
-    image_left = cv2.warpPerspective(image_left,
-                                     ht1
-                                     (width, height))
-    image_right = cv2.warpPerspective(image_right,
-                                      ht2
-                                      (width, height))
-
-    return image_left, image_right
+def nothing(_):
+    pass
 
 
 def main():
@@ -70,61 +29,78 @@ def main():
     left_video = cv2.VideoCapture("test_data/videos/HNI_0054_left/%03d.png")
     right_video = cv2.VideoCapture("test_data/videos/HNI_0054_right/%03d.png")
 
-    # Set up a SIFT feature matcher
-    sift = cv2.SIFT()
+    # StereoSGBM values
+    minDisparity = 0
+    numDisparities = 64
+    SADWindowSize = 5
+    P1 = 8 * 3 * SADWindowSize ** 2
+    P2 = 32 * 3 * SADWindowSize ** 2
+    disp12MaxDiff = -1
 
-    # Set up the disparity calculator
-    stereo = cv2.StereoSGBM(minDisparity=16,
-    numDisparities=96,
-    SADWindowSize=3,
-    uniquenessRatio=10,
-    speckleWindowSize=100,
-    speckleRange=32,
-    disp12MaxDiff=1,
-    P1=216,
-    P2=864,
-    fullDP=False)
+    # Tuner GUI
+    cv2.namedWindow('tuner')
+    cv2.createTrackbar('minDisparity', 'tuner', minDisparity, 100, nothing)
+    cv2.createTrackbar('numDisparities', 'tuner', numDisparities, 2048, nothing)
+    cv2.createTrackbar('SADWindowSize', 'tuner', SADWindowSize, 19, nothing)
+    cv2.createTrackbar('P1', 'tuner', P1, 1000, nothing)
+    cv2.createTrackbar('P2', 'tuner', P2, 10000, nothing)
+
+    # Block matcher
+    stereo = cv2.StereoSGBM(minDisparity, numDisparities, SADWindowSize,
+                            P1, P2, disp12MaxDiff)
 
     # Play the left video
     #ret, frame = left_video.read()
     #while ret is True:
-    #    cv2.imshow("frame", frame)
-    #    if cv2.waitKey(1) & 0xFF == ord('q'):
-    #        break
-    #    ret, frame = left_video.read()
+        #cv2.imshow("frame", frame)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
+        #ret, frame = left_video.read()
 
     # Play the right video
     #ret, frame = right_video.read()
     #while ret is True:
-    #    cv2.imshow("frame", frame)
-    #    if cv2.waitKey(1) & 0xFF == ord('q'):
-    #        break
-    #    ret, frame = right_video.read()
+        #cv2.imshow("frame", frame)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
+        #ret, frame = right_video.read()
 
     ret, frame_left = left_video.read()
     ret, frame_right = right_video.read()
-    while ret is True:
-    #frame_left, frame_right = _rectify_pair(sift,
-    #                                        frame_left,
-    #                                        frame_right)
-        disparity = stereo.compute(frame_left,
-                                    frame_right).astype(np.float32) / 16.0
-        disparity = np.uint8(disparity)
+    while True:
+        while ret is True:
+            disparity = stereo.compute(frame_left,
+                                        frame_right).astype(np.float32) / 16
+            disparity = np.uint8(disparity)
+            cv2.imshow('tuner', disparity)
+            #disparity = np.float32(disparity)
+            #displayDepth('tuner', disparity)
 
-        cv2.imshow('disparity', disparity)
-        cv2.waitKey(1)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            k = cv2.waitKey(1) & 0xFF
+            if k == 27:
+                break
 
+            # Update based on GUI values
+            minDisparity = cv2.getTrackbarPos('minDisparity', 'tuner')
+            numDisparities = (cv2.getTrackbarPos('numDisparities', 'tuner') / 16) * 16
+            SADWindowSize = cv2.getTrackbarPos('SADWindowSize', 'tuner')
+            P1 = cv2.getTrackbarPos('P1', 'tuner')
+            P2 = cv2.getTrackbarPos('P2', 'tuner')
+
+            stereo = cv2.StereoSGBM(minDisparity, numDisparities, SADWindowSize,
+                                    P1, P2, disp12MaxDiff)
+           
+            print minDisparity, numDisparities, SADWindowSize, P1, P2
+
+            ret, frame_left = left_video.read()
+            ret, frame_right = right_video.read()
+        left_video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
+        right_video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
         ret, frame_left = left_video.read()
         ret, frame_right = right_video.read()
 
-
     # Destroy all windows
     cv2.destroyAllWindows()
-
-    # Convert the disparity image into a single channel image
-    #disparity = np.uint8(disparity)
 
 if __name__ == '__main__':
     main()
