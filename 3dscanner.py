6b847d92b122678fe4c0bb18dfb133b38a2dea45
threearmedgupta <nishant@Nishants-MacBook-Air.local>
2014-11-20 00:25:19 -0600

    point cloud generation

--- 3dscanner.py
+++ 3dscanner.py
@@ -64,67 +64,117 @@ def _rectify_pair(sift, image_left, image_right):
 
     return image_left, image_right
 
-
-def main():
-    # Open the left and right streams
-    left_video = cv2.VideoCapture("test_data/videos/HNI_0054_left/%03d.png")
-    right_video = cv2.VideoCapture("test_data/videos/HNI_0054_right/%03d.png")
-
-    # Set up a SIFT feature matcher
-    sift = cv2.SIFT()
-
-    # Set up the disparity calculator
-    stereo = cv2.StereoSGBM(minDisparity=16,
-    numDisparities=96,
-    SADWindowSize=3,
-    uniquenessRatio=10,
-    speckleWindowSize=100,
-    speckleRange=32,
-    disp12MaxDiff=1,
-    P1=216,
-    P2=864,
-    fullDP=False)
-
-    # Play the left video
-    #ret, frame = left_video.read()
-    #while ret is True:
-    #    cv2.imshow("frame", frame)
-    #    if cv2.waitKey(1) & 0xFF == ord('q'):
-    #        break
-    #    ret, frame = left_video.read()
-
-    # Play the right video
-    #ret, frame = right_video.read()
-    #while ret is True:
-    #    cv2.imshow("frame", frame)
-    #    if cv2.waitKey(1) & 0xFF == ord('q'):
-    #        break
-    #    ret, frame = right_video.read()
-
+def get_avg_disparity(left_video, right_video):
     ret, frame_left = left_video.read()
     ret, frame_right = right_video.read()
-    while ret is True:
-    #frame_left, frame_right = _rectify_pair(sift,
-    #                                        frame_left,
-    #                                        frame_right)
-        disparity = stereo.compute(frame_left,
-                                    frame_right).astype(np.float32) / 16.0
-        disparity = np.uint8(disparity)
 
-        cv2.imshow('disparity', disparity)
+    count = 0
+    avg_disparity = np.zeros((240, 480), np.uint8)
+    
+    while ret is True:
+        #frame_left, frame_right = _rectify_pair(sift,
+        #                                        frame_left,
+        #                                        frame_right)
+        
+        #forms average disparity
+        count = count + 1
+        disparity = stereo.compute(frame_left,
+                                   frame_right).astype(np.float32) / 16.0
+        avg_disparity = ((avg_disparity * (count-1)) + disparity)/count
+        print avg_disparity
+
+        cv2.imshow('disparity', np.uint8(disparity))
         cv2.waitKey(1)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+           break
 
         ret, frame_left = left_video.read()
         ret, frame_right = right_video.read()
-
-
+    
     # Destroy all windows
     cv2.destroyAllWindows()
+    cv2.imshow('totaldisparity', avg_disparity)
+    cv2.waitKey(0)
 
-    # Convert the disparity image into a single channel image
-    #disparity = np.uint8(disparity)
+    return avg_disparity
+
+def point_cloud(disparity_image, image_left, focal_length):
+    """Create a point cloud from a disparity image and a focal length.
+        Arguments:
+        disparity_image: disparities in pixels.
+        image_left: BGR-format left stereo image, to color the points.
+        focal_length: the focal length of the stereo camera, in pixels.
+        Returns:
+        A string containing a PLY point cloud of the 3D locations of the
+        pixels, with colors sampled from left_image. You may filter low-
+        disparity pixels or noise pixels if you choose.
+        """
+    h, w = image_left.shape[:2]
+    Q = np.float32([[1, 0,  0, w / 2],
+                    [0, -1,  0,  h / 2],  # turn points 180 deg around x-axis,
+                    [0, 0, focal_length,  0],  # so that y-axis looks up
+                    [0, 0,  0,  1]])
+                    
+    # reproject image points to 3D space, compute the colors
+    points = cv2.reprojectImageTo3D(disparity_image, Q)
+    colors = cv2.cvtColor(image_left, cv2.COLOR_BGR2RGB)
+    mask = disparity_image > disparity_image.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+    
+    # write PLY string data to StringIO object and return the contents
+    cloud = StringIO.StringIO()
+    verts = np.hstack([out_points, out_colors])
+    cloud.write(ply_header % dict(vert_num=len(verts)))
+    np.savetxt(cloud, verts, '%f %f %f %d %d %d')
+    
+    return cloud.getvalue()
+
+def main():
+    for i in ["0", "45", "90", "135", "180", "225", "270", "315"]:
+        # Open the left and right streams
+        left_video = cv2.VideoCapture("test_data/videos/HNI_" + i + "0054_left/$03d.png")
+        right_video = cv2.VideoCapture("test_data/videos/HNI_" + i + "0054_right/%03d.png")
+
+        # Set up a SIFT feature matcher
+        sift = cv2.SIFT()
+
+        # Set up the disparity calculator
+        stereo = cv2.StereoSGBM(minDisparity=16,
+        numDisparities=96,
+        SADWindowSize=3,
+        uniquenessRatio=10,
+        speckleWindowSize=100,
+        speckleRange=32,
+        disp12MaxDiff=1,
+        P1=216,
+        P2=864,
+        fullDP=False)
+
+        # Play the left video
+        #ret, frame = left_video.read()
+        #while ret is True:
+        #    cv2.imshow("frame", frame)
+        #    if cv2.waitKey(1) & 0xFF == ord('q'):
+        #        break
+        #    ret, frame = left_video.read()
+
+        # Play the right video
+        #ret, frame = right_video.read()
+        #while ret is True:
+        #    cv2.imshow("frame", frame)
+        #    if cv2.waitKey(1) & 0xFF == ord('q'):
+        #        break
+        #    ret, frame = right_video.read()
+        
+        ret, frame_left = left_video.read()
+        disparity = get_avg_disparity(left_video, right_video) #Calculates disparity average of view
+        
+        #focal length must be computed and inserted here
+        ply_string = point_cloud(disparity, frame_left, focal_length) #forms point cloud
+
+    #JOIN POINT CLOUDS HERE:
 
 if __name__ == '__main__':
     main()
